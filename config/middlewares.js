c9120febe8ca8ffa25a1124abcf20f703e4d45f9
2D\odie.jamsranjav <odie.jamsranjav@slalom.com>
2024-01-22 10:49:34 -0600

    cors added for the backend

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,19 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["https://ecommerce-6fbpn3r3k-odgerels-projects-4553dc96.vercel.app/"], // Replace with your Vercel app URL
+      // You can specify more CORS options here as needed
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
